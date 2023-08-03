1536c3387021b43dcbeb6c275d90a538d6e9a8c6
RAHUL KUMAR <rahulpatna89@gmail.com>
2023-08-04 02:04:18 +0530

    Update MediCureTestNgReport.java

--- src/test/java/com/project/staragile/MediCureTestNgReport.java
+++ src/test/java/com/project/staragile/MediCureTestNgReport.java
@@ -8,6 +8,18 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.io.File;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class MediCureTestNgReport{
   
           @Test
