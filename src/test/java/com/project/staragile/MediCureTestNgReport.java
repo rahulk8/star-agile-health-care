a63a4464cc3918da4e4508c333ce58d75d332978
RAHUL KUMAR <rahulpatna89@gmail.com>
2023-08-04 03:19:53 +0530

    Update MediCureTestNgReport.java

--- src/test/java/com/project/staragile/MediCureTestNgReport.java
+++ src/test/java/com/project/staragile/MediCureTestNgReport.java
@@ -59,7 +59,7 @@ public class MediCureTestNgReport  {
 	        TakesScreenshot scrShot = ((TakesScreenshot)driver);
 		File screenShot = scrShot.getScreenshotAs(OutputType.FILE);
 		//File destFile = new File ("//home/ubuntu//scr.png");
-		File destFile = new File ("//var/lib/jenkins//scr.png");
+		File destFile = new File ("//var/lib/jenkins/workspace//scr.png");
 		//File destFile = new File ("./scr.png");
 		FileUtils.copyFile(screenShot, destFile); 
 		  
